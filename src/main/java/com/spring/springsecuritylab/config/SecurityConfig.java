c3d88ac5a85ab749976d72b71f939fba98416913
Al Hasan Dhali <alhasandhali@gmail.com>
2026-03-05 20:38:27 +0600

    initial commit (project setup)

---
+++ src/main/java/com/spring/springsecuritylab/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.spring.springsecuritylab.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.formLogin(form -> form.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+        return http.build();
+    }
+}
